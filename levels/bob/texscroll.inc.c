ca907f4a6151611882d03345e650357139bbe8b0
Reonu <danileon95@gmail.com>
2020-08-03 05:12:47 +0100

    idk

--- levels/bob/texscroll.inc.c
+++ levels/bob/texscroll.inc.c
@@ -1,47 +1,3 @@
-void scroll_bob_dl_Termina_mesh_vtx_49() {
-	int i = 0;
-	int count = 4;
-	int width = 32 * 0x20;
-	int height = 32 * 0x20;
-
-	static int currentX = 0;
-	int deltaX;
-	Vtx *vertices = segmented_to_virtual(bob_dl_Termina_mesh_vtx_49);
-
-	deltaX = (int)(0.019999999552965164 * 0x20) % width;
-
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
-	}
-	currentX += deltaX;
-
-}
-void scroll_bob_dl_Fountain_001_mesh_vtx_3() {
-	int i = 0;
-	int count = 4;
-	int width = 32 * 0x20;
-	int height = 32 * 0x20;
-
-	static int currentX = 0;
-	int deltaX;
-	Vtx *vertices = segmented_to_virtual(bob_dl_Fountain_001_mesh_vtx_3);
-
-	deltaX = (int)(0.019999999552965164 * 0x20) % width;
-
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
-	}
-	currentX += deltaX;
-
-}
 void scroll_bob_dl_Mountain_mesh_vtx_0() {
 	int i = 0;
 	int count = 32;
@@ -86,33 +42,8 @@ void scroll_bob_dl_Mountain_halo_mesh_vtx_0() {
 	currentY += deltaY;
 
 }
-void scroll_bob_dl_Sea_mesh_vtx_0() {
-	int i = 0;
-	int count = 4;
-	int width = 32 * 0x20;
-	int height = 32 * 0x20;
-
-	static int currentX = 0;
-	int deltaX;
-	Vtx *vertices = segmented_to_virtual(bob_dl_Sea_mesh_vtx_0);
-
-	deltaX = (int)(0.019999999552965164 * 0x20) % width;
-
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
-	}
-	currentX += deltaX;
-
-}
 void scroll_bob() {
-	scroll_bob_dl_Termina_mesh_vtx_49();
-	scroll_bob_dl_Fountain_001_mesh_vtx_3();
 	scroll_bob_dl_Mountain_mesh_vtx_0();
 	scroll_bob_dl_Mountain_halo_mesh_vtx_0();
-	scroll_bob_dl_Sea_mesh_vtx_0();
 
 }
